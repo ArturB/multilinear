1ed4a6211776fc21c95bfc9dddee0bfa8a3c52fb
ArturB <artur.brodzki@gmail.com>
2017-03-03 21:39:44 +0100

    hoogle

--- .stack-work/dist/ca59d0ab/build/autogen/cabal_macros.h
+++ .stack-work/dist/ca59d0ab/build/autogen/cabal_macros.h
@@ -7,12 +7,12 @@
   (major1) == 0 && (major2) <  1 || \
   (major1) == 0 && (major2) == 1 && (minor) <= 0)
 
-/* package base-4.9.0.0 */
-#define VERSION_base "4.9.0.0"
+/* package base-4.9.1.0 */
+#define VERSION_base "4.9.1.0"
 #define MIN_VERSION_base(major1,major2,minor) (\
   (major1) <  4 || \
   (major1) == 4 && (major2) <  9 || \
-  (major1) == 4 && (major2) == 9 && (minor) <= 0)
+  (major1) == 4 && (major2) == 9 && (minor) <= 1)
 
 /* package binary-0.8.3.0 */
 #define VERSION_binary "0.8.3.0"
@@ -42,12 +42,12 @@
   (major1) == 0 && (major2) <  5 || \
   (major1) == 0 && (major2) == 5 && (minor) <= 7)
 
-/* package directory-1.2.6.2 */
-#define VERSION_directory "1.2.6.2"
+/* package directory-1.3.0.0 */
+#define VERSION_directory "1.3.0.0"
 #define MIN_VERSION_directory(major1,major2,minor) (\
   (major1) <  1 || \
-  (major1) == 1 && (major2) <  2 || \
-  (major1) == 1 && (major2) == 2 && (minor) <= 6)
+  (major1) == 1 && (major2) <  3 || \
+  (major1) == 1 && (major2) == 3 && (minor) <= 0)
 
 /* package zlib-0.6.1.2 */
 #define VERSION_zlib "0.6.1.2"
@@ -56,19 +56,19 @@
   (major1) == 0 && (major2) <  6 || \
   (major1) == 0 && (major2) == 6 && (minor) <= 1)
 
-/* tool alex-3.1.7 */
-#define TOOL_VERSION_alex "3.1.7"
+/* tool alex-3.2.1 */
+#define TOOL_VERSION_alex "3.2.1"
 #define MIN_TOOL_VERSION_alex(major1,major2,minor) (\
   (major1) <  3 || \
-  (major1) == 3 && (major2) <  1 || \
-  (major1) == 3 && (major2) == 1 && (minor) <= 7)
+  (major1) == 3 && (major2) <  2 || \
+  (major1) == 3 && (major2) == 2 && (minor) <= 1)
 
-/* tool cpphs-1.20.2 */
-#define TOOL_VERSION_cpphs "1.20.2"
+/* tool cpphs-1.20.3 */
+#define TOOL_VERSION_cpphs "1.20.3"
 #define MIN_TOOL_VERSION_cpphs(major1,major2,minor) (\
   (major1) <  1 || \
   (major1) == 1 && (major2) <  20 || \
-  (major1) == 1 && (major2) == 20 && (minor) <= 2)
+  (major1) == 1 && (major2) == 20 && (minor) <= 3)
 
 /* tool gcc-5.2.0 */
 #define TOOL_VERSION_gcc "5.2.0"
@@ -77,26 +77,26 @@
   (major1) == 5 && (major2) <  2 || \
   (major1) == 5 && (major2) == 2 && (minor) <= 0)
 
-/* tool ghc-8.0.1 */
-#define TOOL_VERSION_ghc "8.0.1"
+/* tool ghc-8.0.2 */
+#define TOOL_VERSION_ghc "8.0.2"
 #define MIN_TOOL_VERSION_ghc(major1,major2,minor) (\
   (major1) <  8 || \
   (major1) == 8 && (major2) <  0 || \
-  (major1) == 8 && (major2) == 0 && (minor) <= 1)
+  (major1) == 8 && (major2) == 0 && (minor) <= 2)
 
-/* tool ghc-pkg-8.0.1 */
-#define TOOL_VERSION_ghc_pkg "8.0.1"
+/* tool ghc-pkg-8.0.2 */
+#define TOOL_VERSION_ghc_pkg "8.0.2"
 #define MIN_TOOL_VERSION_ghc_pkg(major1,major2,minor) (\
   (major1) <  8 || \
   (major1) == 8 && (major2) <  0 || \
-  (major1) == 8 && (major2) == 0 && (minor) <= 1)
+  (major1) == 8 && (major2) == 0 && (minor) <= 2)
 
-/* tool haddock-2.17.2 */
-#define TOOL_VERSION_haddock "2.17.2"
+/* tool haddock-2.17.3 */
+#define TOOL_VERSION_haddock "2.17.3"
 #define MIN_TOOL_VERSION_haddock(major1,major2,minor) (\
   (major1) <  2 || \
   (major1) == 2 && (major2) <  17 || \
-  (major1) == 2 && (major2) == 17 && (minor) <= 2)
+  (major1) == 2 && (major2) == 17 && (minor) <= 3)
 
 /* tool happy-1.19.5 */
 #define TOOL_VERSION_happy "1.19.5"
@@ -112,12 +112,12 @@
   (major1) == 0 && (major2) <  67 || \
   (major1) == 0 && (major2) == 67 && (minor) <= 0)
 
-/* tool hsc2hs-0.68 */
-#define TOOL_VERSION_hsc2hs "0.68"
+/* tool hsc2hs-0.68.1 */
+#define TOOL_VERSION_hsc2hs "0.68.1"
 #define MIN_TOOL_VERSION_hsc2hs(major1,major2,minor) (\
   (major1) <  0 || \
   (major1) == 0 && (major2) <  68 || \
-  (major1) == 0 && (major2) == 68 && (minor) <= 0)
+  (major1) == 0 && (major2) == 68 && (minor) <= 1)
 
 /* tool hscolour-1.24 */
 #define TOOL_VERSION_hscolour "1.24"
@@ -140,7 +140,7 @@
   (major1) == 2 && (major2) <  25 || \
   (major1) == 2 && (major2) == 25 && (minor) <= 0)
 
-#define CURRENT_COMPONENT_ID "Tensor-0.1.0.0-K5QdifOX00t2w9C27ImRWh"
+#define CURRENT_COMPONENT_ID "Tensor-0.1.0.0-DFCluZsp6XcFZvvYzC4IXO"
 
-#define CURRENT_PACKAGE_KEY "Tensor-0.1.0.0-K5QdifOX00t2w9C27ImRWh"
+#define CURRENT_PACKAGE_KEY "Tensor-0.1.0.0-DFCluZsp6XcFZvvYzC4IXO"
 
