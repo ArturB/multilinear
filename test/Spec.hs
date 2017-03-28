6cfa9dab0a79e30746ea1d18f6b2bf65096ec614
ArturB <artur.brodzki@gmail.com>
2017-03-28 20:35:05 +0200

    fast test

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,13 +17,13 @@ import           Multilinear
 import           Prelude     as P
 
 m1 :: Tensor Int Int
-m1 = tensor ("i",[500]) ("j",[500]) $ \[i] [j] -> i+j
+m1 = tensor ("i",[100]) ("j",[100]) $ \[i] [j] -> i+j
 
 m2 :: Tensor Int Int
-m2 = tensor ("j",[500]) ("k",[500]) $ \[i] [j] -> i+j
+m2 = tensor ("j",[100]) ("k",[100]) $ \[i] [j] -> i+j
 
 v :: Tensor Int Int
-v = tensor ("k",[500]) ([],[]) $ \[k] _ -> k
+v = tensor ("k",[100]) ([],[]) $ \[k] _ -> k
 
 main :: IO ()
 main = do
