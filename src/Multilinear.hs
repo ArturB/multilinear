9c9aeed5dac4ae75b942a33b93a8623d0d680148
ArturB <artur.brodzki@gmail.com>
2017-04-06 22:51:10 +0200

    tutorial

--- src/Multilinear.hs
+++ src/Multilinear.hs
@@ -1,27 +1,146 @@
 {-|
 Module      : Multilinear
-Description : Provides efficient and generic implementation of linear algebra operation using Ricci - Einstein tensor formalism
+Description : A (multi)linear algbra library. 
 Copyright   : (c) Artur M. Brodzki, 2017
 License     : GPL-3
 Maintainer  : artur.brodzki@gmail.com
 Stability   : experimental
 Portability : Windows/POSIX
 
+Multilinear library provides efficient and terse way to deal with linear algebra in Haskell. It is based on concept of tensor - a multidimensional indexed array of numbers. Vectors, matrices and linear functionals are examples of low-order tensors. 
 
+= Quick tutorial
 
-Examples:
+Tensors are indexed, each having one or several indices numbering its components. A scalar has no indices, as being simply a value. A vector has one index, because it is a one-dimensional list of numbers. A matrix has two indices, being a bidimensional table of numbers. Tensors are arbitrarily - dimensional, so you can have a vector of matrices (third-order tensor) and so on. Such tensors are especially useful when dealing with more complex task, such as programming neural networks e.g. for deep learning. 
+
+Index may be either __lower (contravariant)__ or __upper (covariant)__, depending on its function. 
+
+A vector has one __upper__ or __contravariant__ index, because it represents a value, point in a space. A linear functional (called in this library as a __form__) is represented as a list of numbers, similar to vector, but it does not indicate a value, but rather a coefficients of __linear map__  or __transformation__ that takes a vector and returns a number, simply by calculating a linear combination of vector components with weights being a linear functional coefficients. So elements of linear functional are indexed by __lower__ or __covariant__ index. 
+
+A matrix also represents a linear transformation, similar to linear functional, but it take a vector and return a vector, not a scalar. So matrix is simply a bunch of linear functionals - each one is a matrix row and returns a one element of resulting vector. These linear functionals are also indexed - of course by an contravariant index, as vector, which is returned by a matrix. So matrix is a tensor with two indices - one uppper (contravariant) and one lower (covariant).
+
+A dot product is - what a surprise! - a linear transformation. But it is different from matrix - it takes two vectors to multiplicate and returns one number. So it needs two lower (covariant) indices. In fact, a dot product is represented as a bidimensional table of numbers - as matrix - but with two lower indices, instead of one upper and one lower. Tensors with several lower indices are called n-forms. Actually, many linear maps that takes two vectors and returns a number are represented in such notation. A dot product is a table filled with zeros but having a 1-s in diagonal of the table - it look visually identical like unit matrix, but it isn't a matrix - it's actually a 2-form. In this library dot product is called (as in abstract maths) a __Kronecker delta__ (in "Multilinear.NForm""). 
+
+A cross product takes two vectors and returns a vector of numbers. So, as you can guess, it has three indices - two covariant (lower) and one upper (contravariant). A tensor that corresponds to cross product is called Levi-Civita symbol and is implemented under "Multilinear.Tensor". 
+
+As you see, multidimensional indexed arrays - tensors - provide a unified way to deal with linear algebra - such with data (vectors) and functions operating on them (matrices, n-forms). Using tensors instead of limited number of pre-defined functions, you gain a power of universal formalism, where you can express any linear function you want to deal with. 
+
+When you apply a linear functional to a vector, you compute a linear combination - or a weighted sum - of its components. It is so common operation in linear algebra,
+that it requires a convenient way to note. Einstein (1905) introduced a summation rule:
+
+If you multiply a tensor with lower index __/i/__ by a tensor with upper index with same name __/i__ then the linear combination of its components is automatically computed. The only condition is that an index of vector and index of corresponding linear transformation must have the same name. 
+
+== Examples
 
 @
-Prelude>Matrix.fromIndices "ij" 10 10 $ \i j -> i + j
-line 2 
-line 3
+
+>>> v = Vector.fromIndices "i" 5 $ \\i -> i + 2 (vector v has 5 elements and v[i] = i + 2, indexing from 0)
+>>> v
+\<i:5\>
+   | 2
+   | 3
+   | 4
+   | 5
+   | 6
+>>> f = Form.const "i" 5 1 (linear functional has 5 elements and f[i] = 1)
+>>> f
+[i:5] [1,1,1,1,1]
+>>> f * v
+20
 @
 
-prop> a .+ t = t +. a
+If you want to apply a vector to matrix, you must simply multiply them. The only condition is - as you can guess - that lower (indicating that matrix is a linear transformation) index of matrix must have the same name as the upper index of vector (indicating that this vector is a argument of matrix linear transformation; an upper index of matrix here indicates that this linear transformation returns a vector)
+
+@
+
+>>> m = Matrix.fromIndices "ij" 5 5 $ \\i j -> i + j
+>>> m
+\<i:5\>
+   | [j:5] [0,1,2,3,4]
+   | [j:5] [1,2,3,4,5]
+   | [j:5] [2,3,4,5,6]
+   | [j:5] [3,4,5,6,7]
+   | [j:5] [4,5,6,7,8]
+>>> v = Vector.fromIndices "j" 5 $ \\j -> j (vector v has 5 elements and v[j] = j, indexing from 0)
+>>> v
+\<j:5\>
+   | 0
+   | 1
+   | 2
+   | 3
+   | 4
+>>> m * v
+\<i:5\>
+   | 30
+   | 40
+   | 50
+   | 60
+   | 70
+@
+Note, that result vector is indexed with "i" - the same index as uppper index of our matrix. 
+
+If you want to do a matrix multiplication, a lower index of first matrix must have the same name as upper index of second matrix. 
+
+@
+
+>>> m1 = Matrix.fromIndices "ij" 3 5 $ \\i j -> i + j
+>>> m1
+\<i:3\>
+   | [j:5] [0,1,2,3,4]
+   | [j:5] [1,2,3,4,5]
+   | [j:5] [2,3,4,5,6]
+>>> m2 = Matrix.fromIndices "jk" 5 4 $ \\j -> i + j (vector v has 5 elements and v[j] = j, indexing from 0)
+>>> m2
+\<j:5\>
+   | [k:4] [0,1,2,3]
+   | [k:4] [1,2,3,4]
+   | [k:4] [2,3,4,5]
+   | [k:4] [3,4,5,6]
+   | [k:4] [4,5,6,7]
+>>> m1 * m2
+\<i:3\>
+   | [k:4] [30,40,50,60]
+   | [k:4] [40,50,60,70]
+   | [k:4] [50,60,70,80]
+@
+
+Note, that rule that first matrix lower index must be the same as the second matrix upper index corresponeds to the fact, that to multiply two matrices, the widht of first matrix must be the same as width of the second matrix. The rule of matrix multiplication guarantees, that this operation is equivalent to linear functions composition. 
+
+The dot product of vectors may be done by simply making one of vectors with lower index, or - more correctly - by applying two vectors to Kronecker delta:
+
+@
+
+>>> v1 = Vector.fromIndices "i" 5 $ \\i -> i + 2 (vector v has 5 elements and v[i] = i + 2, indexing from 0)
+>>> v1
+\<i:5\>
+   | 2
+   | 3
+   | 4
+   | 5
+   | 6
+>>> v2 = Vector.fromIndices "j" 5 $ \\j -> j (vector v has 5 elements and v[j] = j, indexing from 0)
+>>> v2
+\<j:5\>
+   | 0
+   | 1
+   | 2
+   | 3
+   | 4
+>>> v1 * (lower "j" v2)
+50
+>>> NForm.dot 5 "ij" * v1 * v2 (A Kronecker delta - representing a dot product - of size 5 with indices \"i\" and \"j\" is multiplied by v1 and v2)
+50
+
+@
+
+If you want to know more about linear algebra and Einstein convention, read Wikipedia: 
+
+- <https://en.wikipedia.org/wiki/Matrix_(mathematics)>
+- <https://en.wikipedia.org/wiki/Covariance_and_contravariance_of_vectors>
+- <https://en.wikipedia.org/wiki/Einstein_notation>
 
 -}
 
-{-# LANGUAGE GADTs  #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE Strict #-}
 {-# OPTIONS_GHC -O2 #-}
@@ -39,23 +158,18 @@ import Data.Bits
 {-| Multidimensional array treated as multilinear map - tensor -}
 class (
   Num (t i a),        -- Tensors may be added, subtracted and multiplicated
-  Integral i,         -- Indices of tensors must be of integral type that allow to sum them finitely
+  Integral i,         -- Indices of tensors must be of integral type
   Bits (t i a),       -- Also bit operations can be performed on tensors
-  Functor (t i)       -- Tensor is a multidimensional array and should be a Functor for convenience
+  Functor (t i)       -- Tensor should be a Functor for convenience
   ) => Multilinear t i a where
 
-    {-| Add scalar @a@ left to tensor @t@ producing tensor @a + t@ -}
-    -- prop> a .+ t = t +. a
+    {-| Add scalar @a@ left to each element of tensor @t@ -}
     infixl 7 .+
-    (.+) :: a     -- ^ Scalar a
-         -> t i a -- ^ Tensor t
-         -> t i a -- ^ Tensor a + t
+    (.+) :: a -> t i a -> t i a
 
-    {-| Subtract scalar left -}
+    {-| Subtract each element of tensor @t@ from scalar scalar left -}
     infixl 7 .-
-    (.-) :: a       
-         -> t i a 
-         -> t i a
+    (.-) :: a -> t i a -> t i a
 
     {-| Multiply by scalar left-}
     infixl 8 .*
@@ -172,12 +286,6 @@ class (
 
     {-| Accessing tensor elements |-}
     el :: [String] -> t i a -> [i] -> t i a
-
-    {-| Simple filtering |-}
-    --filter :: (a -> Bool) -> t i a -> Maybe (t i a)
-
-    {-| Filtering with indices |-}
-    --iFilter :: [String] -> ([i] -> a -> Bool) -> t i a -> Maybe (t i a)
 
     {-| Simple mapping |-}
     map :: (a -> b) -> t i a -> t i b
