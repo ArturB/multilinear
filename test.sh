8d617a28218c3c1b8aaf6a67139a5e3921eb2bae
Artur Brodzki <artur@brodzki.org>
2018-11-23 14:13:23 +0100

    Build 18327.47600

--- test.sh
+++ test.sh
@@ -5,7 +5,9 @@
 let "TODAY_SEC = $( date +%s ) % 86400"
 BUILD_ID="Build $( date +%y%j ).$TODAY_SEC"
 
-if stack test --coverage && git add -A > /dev/null && git commit -qm "$BUILD_ID" && git push -q ; then
+if stack test --coverage ; then
+    echo "Pushing to git..."
+    git add -A > /dev/null && git commit -qm "$BUILD_ID" && git push -q
     echo -e "\u001b[32mAll done!\u001b[0m"
 else
     echo -e "\u001b[31mSome tests didn't pass!\u001b[0m"
