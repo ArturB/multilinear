395bcb903aa36dc078bd4b6e835fbce3effc269b
Artur Brodzki <artur@brodzki.org>
2018-11-24 14:41:23 +0100

    Build 18328.49231

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ echo -e "Building doc...\n" &&
 stack haddock && 
 echo -e "Uploading to Hackage...\n" && 
 stack sdist && 
-stack upload . && 
-echo -e "\u001b[32mAll done!\u001b[0m"
+stack upload . && echo -e "\u001b[32mAll done!\u001b[0m"
