304b64642ca16846f2463669662f9df29dae6d54
Artur Brodzki <artur@brodzki.org>
2018-12-03 13:28:46 +0100

    Untested build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-# Re-build whole project, with documentation
-if stack test --coverage --no-run-tests ; then
+# Re-build whole project, with documentation 
+if stack build --fast --test --coverage --no-run-tests --bench --no-run-benchmarks; then
     echo -e "Build successful!\nPushing changes to git..."
     ( git add -A > /dev/null && git commit -qm "Untested build $BUILD_ID" && git pull -q && git push -q ) &
     echo -e "All done!"
