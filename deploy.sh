9ada71754151bd3d8e169d071e5f51e95c6af48b
Artur Brodzki <artur@brodzki.org>
2018-12-03 14:05:39 +0100

    Benchmark build

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,1 @@
-./test.sh && 
-echo -e "\nBuilding doc..." && 
-stack haddock --coverage && 
-echo -e "\nUploading to Hackage..." && 
-stack sdist && 
-stack upload .
+./test.sh && stack sdist && stack upload .
