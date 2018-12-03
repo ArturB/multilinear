e1276a2438c77898f7cebe7c4484e16969c1a8f8
Artur Brodzki <artur@brodzki.org>
2018-12-03 13:33:49 +0100

    Untested build

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-# Re-build whole project, with documentation 
-if stack build --fast --test --coverage --no-run-tests --bench --no-run-benchmarks; then
+# Build whole project: library, tests, benchmarks, documentation 
+if stack build --fast --test --coverage --no-run-tests --bench --no-run-benchmarks --haddock; then
     echo -e "Build successful!\nPushing changes to git..."
     ( git add -A > /dev/null && git commit -qm "Untested build $BUILD_ID" && git pull -q && git push -q ) &
     echo -e "All done!"
     exit 0
+else
+    echo -e "\u001b[31mBuild failure!\u001b[0m"
 fi
