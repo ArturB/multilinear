7933ff75ca5b02b8be50e9cd22955d0820f76e4b
Artur Brodzki <artur@brodzki.org>
2019-01-04 18:26:41 +0100

    Build 19004.62800

--- vcs.sh
+++ vcs.sh
@@ -17,17 +17,16 @@ done
 
 # get current branch name
 CUR_BRANCH=$( git rev-parse --abbrev-ref HEAD )
-DAILY_BRANCH=$( date +%d-%m )
 USER=$( whoami | tr '[:upper:]' '[:lower:]' )
+DAILY_BRANCH="daily-$USER-$BUILD_ID"
 
 # based on build state, commit to master or to daily branch
 if [ $CUR_BRANCH == "master" ] ; then
     if [ "$TESTED" != "" ] ; then
         ( git commit -aqm "Build $BUILD_ID" && git pull -q && git push -q ) &
     else
-        git branch "daily-$USER-$DAILY_BRANCH"
         git stash 
-        git checkout -b "daily-$USER-$BUILD_ID"
+        git checkout -b "DAILY_BRANCH"
         git stash pop
         if "$COMPILED" ; then
             ( git commit -qm "Build $BUILD_ID" && git push -q ) &
