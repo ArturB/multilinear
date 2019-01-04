ac81a95a0b1ac7d8beb1ce8ebfb8d6a2d73a4b4c
Artur Brodzki <artur@brodzki.org>
2019-01-04 18:20:51 +0100

    vcs merge

--- vcs.sh
+++ vcs.sh
@@ -18,20 +18,22 @@ done
 # get current branch name
 CUR_BRANCH=$( git rev-parse --abbrev-ref HEAD )
 DAILY_BRANCH=$( date +%d-%m )
+USER=$( whoami | tr '[:upper:]' '[:lower:]' )
 
 # based on build state, commit to master or to daily branch
 if [ $CUR_BRANCH == "master" ] ; then
     if [ "$TESTED" != "" ] ; then
         ( git commit -aqm "Build $BUILD_ID" && git pull -q && git push -q ) &
     else
-        git branch "daily-$DAILY_BRANCH"
+        git branch "daily-$USER-$DAILY_BRANCH"
         git stash 
-        git checkout "daily-$DAILY_BRANCH"
+        git checkout -b "daily-$USER-$BUILD_ID"
         git stash pop
         if "$COMPILED" ; then
-            ( git commit -aqm "Build $BUILD_ID" && git pull -q && git push -q ) &
+            ( git commit -qm "Build $BUILD_ID" && git push -q ) &
         else
-            ( git commit -aqm "Temp $BUILD_ID" && git pull -q && git push -q ) &
+            ( git commit -qm "Temp $BUILD_ID"  && git push -q ) &
         fi
     fi
 fi
+
