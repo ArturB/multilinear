b618b51d01b98e9e333afa67a815d85894979e6d
Artur Brodzki <artur@brodzki.org>
2019-01-04 18:41:43 +0100

    Build 19004.63703

---
+++ vcs.sh
@@ -0,0 +1,51 @@
+source id-build.sh
+
+# parse arguments
+COMPILED=""
+TESTED=""
+
+i=0
+while [ $i -le $# ] ; do
+    case $1 in
+        --compiled) COMPILED="true" ; ;; 
+        --tested)   TESTED="true" ;   ;; 
+        *) echo Unknown option $1 ;   ;; 
+    esac
+    let "i=$i+1"
+    shift
+done
+
+# get current branch name
+CUR_BRANCH=$( git rev-parse --abbrev-ref HEAD )
+USER=$( whoami | tr '[:upper:]' '[:lower:]' )
+DAILY_BRANCH="daily-$USER-$BUILD_ID"
+
+# based on build state, commit to master or to daily branch
+
+# ON MASTER BRANCH
+if [ $CUR_BRANCH == "master" ] ; then
+    if [ "$TESTED" != "" ] ; then
+        ( git commit -aqm "Build $BUILD_ID" && git pull -q && git push -q ) &
+    else
+        git add -A && git stash 
+        git checkout -b "$DAILY_BRANCH"
+        git stash pop
+        if [ "$COMPILED" != "" ] ; then
+            ( git commit -aqm "Build $BUILD_ID" && git push -q --set-upstream origin "$DAILY_BRANCH" ) &
+        else
+            ( git commit -aqm "Temp $BUILD_ID"  && git push -q --set-upstream origin "$DAILY_BRANCH" ) &
+        fi
+    fi
+
+# ON DAILY BRANCH
+else
+    if [ "$TESTED" != "" ] ; then
+        ( git add -A && git commit -qm "Build $BUILD_ID" && git pull -q && git push -q ) &
+    else
+        if [ "$COMPILED" != "" ] ; then
+            ( git add -A && git commit -qm "Build $BUILD_ID" && git push -q --set-upstream origin "$DAILY_BRANCH" ) &
+        else
+            ( git add -A && git commit -qm "Temp $BUILD_ID"  && git push -q --set-upstream origin "$DAILY_BRANCH" ) &
+        fi
+    fi
+fi
