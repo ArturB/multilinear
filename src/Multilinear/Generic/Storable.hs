d6f4ab611dc9fea17889e5bdc534c519b7b74906
Artur Brodzki <artur@brodzki.org>
2019-01-05 20:21:56 +0100

    Temp 19005.69716

--- src/Multilinear/Generic/Storable.hs
+++ src/Multilinear/Generic/Storable.hs
@@ -18,7 +18,7 @@ import           Data.List
 import qualified Data.Vector                   as Boxed
 import qualified Data.Vector.Storable          as StorableV
 import           GHC.Generics
-import           Multilinear.Class
+import           Multilinear.Class             as Multilinear
 import qualified Multilinear.Index             as Index
 import qualified Multilinear.Index.Finite      as Finite
 
