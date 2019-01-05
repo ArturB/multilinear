1fc13d88bd19f2adad1c50893806c27dbe94c4a5
Artur Brodzki <artur@brodzki.org>
2019-01-05 19:22:58 +0100

    Temp 19005.66175

--- test/all/GPU.hs
+++ test/all/GPU.hs
@@ -89,144 +89,32 @@ showTest = MultilinearTests.showTest
 vectorConstructorValues :: Char -> Positive (Small Int) -> Tensor Double -> Bool
 vectorConstructorValues = ConstructorsTests.vectorConstructorValues
 
+formConstructorValues :: Char -> Positive (Small Int) -> Tensor Double -> Bool
+formConstructorValues = ConstructorsTests.formConstructorValues
 
+matrixConstructorValues :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Tensor Double -> Bool
+matrixConstructorValues = ConstructorsTests.matrixConstructorValues
 
+nVectorConstructorValues :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Tensor Double -> Bool
+nVectorConstructorValues = ConstructorsTests.nVectorConstructorValues
 
+nFormConstructorValues :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Tensor Double -> Bool
+nFormConstructorValues = ConstructorsTests.nFormConstructorValues
 
--- | Test generic vector constructor indices
-vectorConstructor :: Char -> Positive (Small Int) -> Bool
-vectorConstructor c s = 
-    let size = getSmall $ getPositive s
-        v :: Tensor Double = Vector.fromIndices [c] size fromIntegral
-        vConst :: Tensor Double = Vector.const [c] size (fromIntegral size)
-    in  v `Multilinear.Class.size` [c] == size && vConst `Multilinear.Class.size` [c] == size
+vectorConstructorError :: Char -> Positive (Small Int) -> Tensor Double -> Property
+vectorConstructorError = ConstructorsTests.vectorConstructorError
 
--- | Test generic form constructor indices
-formConstructor :: Char -> Positive (Small Int) -> Bool
-formConstructor c s = 
-    let size = getSmall $ getPositive s
-        f :: Tensor Double = Form.fromIndices [c] size fromIntegral
-        fConst :: Tensor Double = Form.const [c] size (fromIntegral size)
-    in  f `Multilinear.Class.size` [c] == size && fConst `Multilinear.Class.size` [c] == size
+formConstructorError :: Char -> Positive (Small Int) -> Tensor Double -> Property
+formConstructorError = ConstructorsTests.formConstructorError
 
--- | Test generic matrix constructor indices
-matrixConstructor :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Bool
-matrixConstructor c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = Matrix.fromIndices [c1,c2] size1 size2 (\x y -> fromIntegral x + fromIntegral y)
-        vConst :: Tensor Double = Matrix.const [c1,c2] size1 size2 (fromIntegral size1)
-    in  c1 == c2 || ( v `Multilinear.Class.size` [c1] == size1 && v `Multilinear.Class.size` [c2] == size2 
-              && vConst `Multilinear.Class.size` [c1] == size1 && vConst `Multilinear.Class.size` [c2] == size2 )
+matrixConstructorError :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> t a -> Property
+matrixConstructorError = ConstructorsTests.matrixConstructorError
 
--- | Test generic NVector constructor indices
-nVectorConstructor :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Bool
-nVectorConstructor c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = NVector.fromIndices [c1,c2] [size1,size2] (\[x,y] -> fromIntegral x + fromIntegral y)
-        vConst :: Tensor Double = NVector.const [c1,c2] [size1,size2] (fromIntegral size1)
-    in  c1 == c2 || ( v `Multilinear.Class.size` [c1] == size1 && v `Multilinear.Class.size` [c2] == size2
-              && vConst `Multilinear.Class.size` [c1] == size1 && vConst `Multilinear.Class.size` [c2] == size2 )
+nVectorConstructorError :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> t a -> Property
+nVectorConstructorError = ConstructorsTests.nVectorConstructorError
 
--- | Test generic NForm constructor indices
-nFormConstructor :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Bool
-nFormConstructor c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = NForm.fromIndices [c1,c2] [size1,size2] (\[x,y] -> fromIntegral x + fromIntegral y)
-        vConst :: Tensor Double = NForm.const [c1,c2] [size1,size2] (fromIntegral size1)
-    in  c1 == c2 || ( v `Multilinear.Class.size` [c1] == size1 && v `Multilinear.Class.size` [c2] == size2
-              && vConst `Multilinear.Class.size` [c1] == size1 && vConst `Multilinear.Class.size` [c2] == size2 )
-
--- | Test generic vector constructor indices error
-vectorConstructorError :: Char -> Positive (Small Int) -> Property
-vectorConstructorError c s = 
-    let size = getSmall $ getPositive s
-        v :: Tensor Double = Vector.fromIndices [c,'a'] size fromIntegral
-    in  expectFailure (total v)
-
--- | Test generic form constructor indices error
-formConstructorError :: Char -> Positive (Small Int) -> Property
-formConstructorError c s = 
-    let size = getSmall $ getPositive s
-        f :: Tensor Double = Form.fromIndices [c,'a'] size fromIntegral
-    in  expectFailure (total f)
-
--- | Test generic matrix constructor indices error
-matrixConstructorError :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Property
-matrixConstructorError c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = Matrix.fromIndices [c1,c2,'a'] size1 size2 (\x y -> fromIntegral x + fromIntegral y)
-    in  expectFailure (total v)
-
--- | Test generic NVector constructor indices error
-nVectorConstructorError :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Property
-nVectorConstructorError c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = NVector.fromIndices [c1,c2,'a'] [size1,size2] (\[x,y] -> fromIntegral x + fromIntegral y)
-    in  expectFailure (total v)
-
--- | Test generic NForm constructor indices error
-nFormConstructorError :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Property
-nFormConstructorError c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = NForm.fromIndices [c1,c2,'a'] [size1,size2] (\[x,y] -> fromIntegral x + fromIntegral y)
-    in  expectFailure (total v)
-
--- | Test generic form constructor values
-formConstructorValues :: Char -> Positive (Small Int) -> Bool
-formConstructorValues c s = 
-    let size = getSmall $ getPositive s
-        f :: Tensor Double = Form.fromIndices [c] size fromIntegral
-        fConst :: Tensor Double = Form.const [c] size (fromIntegral size)
-    in  all (\i -> f $$| ([c],[i]) == fromIntegral i) [0.. size - 1] && 
-        all (\i -> fConst $$| ([c],[i]) == fromIntegral size) [0 .. size - 1]
-
--- | Test generic matrix constructor values
-matrixConstructorValues :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Bool
-matrixConstructorValues c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = Matrix.fromIndices [c1,c2] size1 size2 (\x y -> fromIntegral x + fromIntegral y)
-        vConst :: Tensor Double = Matrix.const [c1,c2] size1 size2 (fromIntegral size1)
-    in  c1 == c2 || (
-        all (\(i1,i2) -> v $$| ([c1,c2],[i1,i2]) == fromIntegral i1 + fromIntegral i2) 
-            (pure (,) <*> [0 .. size1 - 1] <*> [0 .. size2 - 1]) && 
-        all (\(i1,i2) -> vConst $$| ([c1,c2],[i1,i2]) == fromIntegral size1) 
-            (pure (,) <*> [0 .. size1 - 1] <*> [0 .. size2 - 1])
-        )
-
--- | Test generic NVector constructor values
-nVectorConstructorValues :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Bool
-nVectorConstructorValues c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = NVector.fromIndices [c1,c2] [size1,size2] (\[x,y] -> fromIntegral x + fromIntegral y)
-        vConst :: Tensor Double = NVector.const [c1,c2] [size1,size2] (fromIntegral size1)
-    in  c1 == c2 || (
-        all (\(i1,i2) -> v $$| ([c1,c2],[i1,i2]) == fromIntegral i1 + fromIntegral i2) 
-            (pure (,) <*> [0 .. size1 - 1] <*> [0 .. size2 - 1]) && 
-        all (\(i1,i2) -> vConst $$| ([c1,c2],[i1,i2]) == fromIntegral size1) 
-            (pure (,) <*> [0 .. size1 - 1] <*> [0 .. size2 - 1])
-        )
-
--- | Test generic NForm constructor values
-nFormConstructorValues :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> Bool
-nFormConstructorValues c1 c2 s1 s2 = 
-    let size1 = getSmall $ getPositive s1
-        size2 = getSmall $ getPositive s2
-        v :: Tensor Double = NForm.fromIndices [c1,c2] [size1,size2] (\[x,y] -> fromIntegral x + fromIntegral y)
-        vConst :: Tensor Double = NForm.const [c1,c2] [size1,size2] (fromIntegral size1)
-    in  c1 == c2 || (
-        all (\(i1,i2) -> v $$| ([c1,c2],[i1,i2]) == fromIntegral i1 + fromIntegral i2) 
-            (pure (,) <*> [0 .. size1 - 1] <*> [0 .. size2 - 1]) && 
-        all (\(i1,i2) -> vConst $$| ([c1,c2],[i1,i2]) == fromIntegral size1) 
-            (pure (,) <*> [0 .. size1 - 1] <*> [0 .. size2 - 1])
-        )
+nFormConstructorError :: Char -> Char -> Positive (Small Int) -> Positive (Small Int) -> t a -> Property
+nFormConstructorError = ConstructorsTests.nFormConstructorError
 
 -- | Check indices preservation if zipWith function
 zipWithTest :: Tensor Double -> Tensor Double -> Bool
