335151513cf7614a792f3e9b27d3786ef34d3930
Artur Brodzki <artur@brodzki.org>
2019-01-05 14:35:43 +0100

    Temp 19005.48943

--- test/all/GPU.hs
+++ test/all/GPU.hs
@@ -34,38 +34,6 @@ defTestN :: Int
 defTestN = 200
 
 
-------------------------------
--- AUXILIARY TEST FUNCTIONS --
-------------------------------
-
-
--- quickCheck with parametrizable tests number
-quickCheckN :: Testable prop => Int -> prop -> IO Result
-quickCheckN n = quickCheckWithResult (Args 
-    Nothing -- ^ Should we replay a previous test? No. 
-    n       -- ^ Maximum number of successful tests before succeeding set to N. 
-    1       -- ^ Maximum number of discarded tests per successful test before giving up - gave up after first failure. 
-    n       -- ^ Size to use for the biggest test cases.
-    True    -- ^ Whether to print anything? yes. 
-    0)      -- ^ Maximum number of shrinks to before giving up. Turn shrinking off.
-
--- | Execute property test and check result:
--- | exit test suite with successs code if no errors occured
--- | exit test suite with failure code if any error occured
-executePropertyTest :: (
-    Testable prop 
-    ) => String -- ^ Tested property name
-      -> Int    -- ^ Number of tests to do
-      -> prop   -- ^ Property to test
-      -> IO ()
-executePropertyTest propName n f = do
-    putStr $ "  Checking " ++ propName ++ " "
-    r <- quickCheckN n f
-    case r of
-        Success _ _ _  -> hFlush stdout
-        _ -> exitFailure
-
-
 ----------------------------------------------------
 -- TESTED TENSOR PROPERTIES FOR SEQUENTIAL TENSOR --
 ----------------------------------------------------
