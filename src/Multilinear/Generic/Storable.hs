10ba526a99a03ffd188e504bd6b136eb8d0faebe
Artur Brodzki <artur@brodzki.org>
2019-01-05 20:17:27 +0100

    Temp 19005.69447

--- src/Multilinear/Generic/Storable.hs
+++ src/Multilinear/Generic/Storable.hs
@@ -39,3 +39,27 @@ data Tensor a where
         tensorsFinite     :: Boxed.Vector (Tensor a)
     } -> Tensor a
     deriving (Eq, Generic)
+
+-- | Print tensor
+instance (
+    Multilinear Tensor a, Show a
+    ) => Show (Tensor a) where
+
+    -- merge errors first and then print whole tensor
+    show = show' . standardize
+        where
+        show' x = case x of
+            -- Scalar is showed simply as its value
+            Scalar v -> show v
+            -- SimpleFinite is shown dependent on its index...
+            SimpleFinite index ts -> show index ++ "S: " ++ case index of
+                -- If index is contravariant, show tensor components vertically
+                Finite.Contravariant _ _ -> "\n" ++ tail (StorableV.foldl' (\string e -> string ++ "\n  |" ++ show e) "" ts)
+                -- If index is covariant or indifferent, show tensor compoments horizontally
+                _                        -> "["  ++ tail (StorableV.foldl' (\string e -> string ++ "," ++ show e) "" ts) ++ "]"
+            -- FiniteTensor is shown dependent on its index...
+            FiniteTensor index ts -> show index ++ "T: " ++ case index of
+                -- If index is contravariant, show tensor components vertically
+                Finite.Contravariant _ _ -> "\n" ++ tail (Boxed.foldl' (\string e -> string ++ "\n  |" ++ show e) "" ts)
+                -- If index is covariant or indifferent, show tensor compoments horizontally
+                _                        -> "["  ++ tail (Boxed.foldl' (\string e -> string ++ "," ++ show e) "" ts) ++ "]"
